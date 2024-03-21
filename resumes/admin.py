700e5a24e4bc64634d81a95a9d84136d487b619e
FlynntKnapp <bruce.stull@gmail.com>
2024-03-21 03:54:10 -0400

    Add 'order' field to 'TechnicalSkill' for sorting by 'order'.

--- resumes/admin.py
+++ resumes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProfessionalName
+from .models import ProfessionalName, TechnicalSkillType, TechnicalSkill
 
 
 @admin.register(ProfessionalName)
@@ -28,6 +28,59 @@ class ProfessionalNameAdmin(admin.ModelAdmin):
                     "suffix",
                 )
             },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "created",
+                    "updated",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+
+@admin.register(TechnicalSkillType)
+class TechnicalSkillTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    list_filter = ["created", "updated"]
+    readonly_fields = ["created", "updated"]
+    fieldsets = (
+        (
+            None,
+            {"fields": ("name",)},
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "created",
+                    "updated",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+
+@admin.register(TechnicalSkill)
+class TechnicalSkillAdmin(admin.ModelAdmin):
+    list_display = ["name", "skill_type", "user", "order"]
+    search_fields = [
+        "user__username",
+        "user__email",
+        "skill_type__name",
+        "name",
+    ]
+    list_filter = ["created", "updated"]
+    readonly_fields = ["created", "updated"]
+    fieldsets = (
+        (
+            None,
+            {"fields": ("user", "skill_type", "name")},
         ),
         (
             "Important Dates",
